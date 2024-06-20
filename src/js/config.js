fb9f45ffd4c1028d5b9447e293074edb40329211
IwuFranklin67 <frankliniwu67@gmail.com>
2024-06-20 18:21:48 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1312d5ba-02cb-409a-97b5-a22ce32905f2';
+export const MODAL_CLOSE_SEC = 2.5;
